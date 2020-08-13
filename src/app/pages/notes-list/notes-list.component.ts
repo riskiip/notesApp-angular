93a7b57cf110fb27212afcb64be0d22ddcda2aaa
riskiip <muhamadrizki19@student.undip.ac.id>
2020-08-13 14:36:43 +0700

    Filtering works fine

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { Note } from "src/app/shared/note.model";
 import { NotesService } from "src/app/shared/notes.service";
 import { trigger, transition, style, animate, query, stagger } from "@angular/animations";
+import { element } from 'protractor';
 
 @Component({
   selector: "app-notes-list",
@@ -75,14 +76,58 @@ import { trigger, transition, style, animate, query, stagger } from "@angular/an
 })
 export class NotesListComponent implements OnInit {
   notes: Note[] = new Array<Note>();
+  filteredNote: Note[] = new Array<Note>();
 
   constructor(private noteService: NotesService) {}
 
   ngOnInit() {
     this.notes = this.noteService.getAll();
+    this.filteredNote = this.notes;
   }
 
   deleteNote(id: number) {
     this.noteService.deleteNote(id);
+  }
+
+  filter(query: string) {
+    query = query.toLowerCase().trim();
+
+    let allResult: Note[] = new Array<Note>();
+
+    //split up the space
+    let terms: string[] = query.split(' ');
+    terms = this.removeDuplicates(terms);
+
+    //compile all relevant notes
+    terms.forEach(term => {
+      let results: Note[] = this.relevantNote(term);
+      //append result
+      allResult = [...allResult, ...results]
+    });
+
+    let unique = this.removeDuplicates(allResult);
+    this.filteredNote = unique;
+  }
+
+  removeDuplicates(arr: Array<any>) : Array<any> {
+    let unique: Set<any> = new Set<any>();
+    arr.forEach(e => unique.add(e));
+    return Array.from(unique);
+  }
+
+  relevantNote(query: string): Array<Note> {
+    query = query.toLowerCase().trim();
+    let relevantNote = this.notes.filter( note => {
+      if (note.title && note.title.toLowerCase().includes(query)) {
+        return true;
+      }
+      if (note.body && note.body.toLowerCase().includes(query)) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    });
+    return relevantNote;
   }
 }
