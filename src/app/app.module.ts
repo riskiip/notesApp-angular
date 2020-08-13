abec52aaf264623c896e51539af797034814a31d
riskiip <muhamadrizki19@student.undip.ac.id>
2020-08-13 10:44:07 +0700

    Animation included

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +21,8 @@ import { NoteDetailsComponent } from './pages/note-details/note-details.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,16 +1,82 @@
-import { Component, OnInit } from '@angular/core';
-import { Note } from 'src/app/shared/note.model';
-import { NotesService } from 'src/app/shared/notes.service';
+import { Component, OnInit } from "@angular/core";
+import { Note } from "src/app/shared/note.model";
+import { NotesService } from "src/app/shared/notes.service";
+import { trigger, transition, style, animate, query, stagger } from "@angular/animations";
 
 @Component({
-  selector: 'app-notes-list',
-  templateUrl: './notes-list.component.html',
-  styleUrls: ['./notes-list.component.scss']
+  selector: "app-notes-list",
+  templateUrl: "./notes-list.component.html",
+  styleUrls: ["./notes-list.component.scss"],
+  animations: [
+    trigger("itemAnim", [
+      transition("void => *", [
+        style({
+          height: 0,
+          opacity: 0,
+          transform: "scale(0.85)",
+          "margin-bottom": 0,
+
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingRight: 0,
+          paddingLeft: 0,
+        }),
+        animate('50ms', style({
+          height: '*',
+          'margin-bottom': '*',
+          paddingTop: '*',
+          paddingBottom: '*',
+          paddingLeft: '*',
+          paddingRight: '*'
+        })),
+        animate(70)
+      ]),
+
+      transition('* => void', [
+        animate(50, style({
+          transform: 'scale(1.05)'
+        })),
+        animate(50, style({
+          transform: 'scale(1)',
+          opacity: 0.75
+        })),
+        animate('120ms ease-out', style({
+          transform: 'scale(0.70)',
+          opacity: 0
+        })),
+
+        // animate spacing include height, padding and margin
+        animate('150ms ease-out', style({
+          height: 0,
+          "margin-bottom": '0',
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingRight: 0,
+          paddingLeft: 0,
+        }))
+      ])
+    ]),
+    trigger('listAnim', [
+      transition('* => *', [
+        query(':enter', [
+          style({
+            opacity: 0,
+            height: 0
+          }),
+          stagger(100, [
+            animate('0.2s ease')
+          ])
+        ], {
+          optional: true
+        })
+      ])
+    ])
+  ],
 })
 export class NotesListComponent implements OnInit {
   notes: Note[] = new Array<Note>();
 
-  constructor(private noteService: NotesService) { }
+  constructor(private noteService: NotesService) {}
 
   ngOnInit() {
     this.notes = this.noteService.getAll();
@@ -19,5 +85,4 @@ export class NotesListComponent implements OnInit {
   deleteNote(id: number) {
     this.noteService.deleteNote(id);
   }
-
 }
